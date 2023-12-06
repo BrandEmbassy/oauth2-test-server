0783d9648d047b48da6683d1cf512054fff1ea48
Matej Simek <matej.simek@nice.com>
2023-12-06 14:59:06 +0100

    secure: logs

--- auth/routes/secure.js
+++ auth/routes/secure.js
@@ -3,15 +3,21 @@ const router = express.Router() // Instantiate a new router
 const DebugControl = require('../utilities/debug.js')
 
 router.get('/', (req,res) => {  // Successfully reached if can hit this :)
-  DebugControl.log.variable({name: 'res.locals.oauth.token', value: res.locals.oauth.token})
-  res.json({
+  DebugControl.log.flow('Secure');
+  DebugControl.log.variable({name: 'res.locals.oauth.token', value: res.locals.oauth.token});
+
+  const body = {
     success: true,
     user: {
       id: String(Math.random() * 1000000000000000000),
       firstName: 'John',
       lastName: 'Doe'
     }
-  })
+  };
+
+  DebugControl.log.variable({name: 'user.id', value: body.user.id});
+
+  res.json(body)
 })
 
 module.exports = router
