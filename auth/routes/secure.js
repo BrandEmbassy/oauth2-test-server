696b05c5821997f592b841d76772eed9ce7eba17
Matej Simek <matej.simek@nice.com>
2023-12-06 14:45:33 +0100

    add user payload to the /secure/ endpoint

--- auth/routes/secure.js
+++ auth/routes/secure.js
@@ -4,7 +4,14 @@ const DebugControl = require('../utilities/debug.js')
 
 router.get('/', (req,res) => {  // Successfully reached if can hit this :)
   DebugControl.log.variable({name: 'res.locals.oauth.token', value: res.locals.oauth.token})
-  res.json({success: true})
+  res.json({
+    success: true,
+    user: {
+      id: Math.random() * 1000000000000000000,
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+  })
 })
 
 module.exports = router
